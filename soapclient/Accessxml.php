415148f30060f4e06020785445e10d3a160604bf
ganesh-webonise <ganesh@weboniselab.com>
2013-07-19 17:48:53 +0530

    added server file and done crete ,modify delete functionality

--- soapclient/Accessxml.php
+++ soapclient/Accessxml.php
@@ -1,5 +1,40 @@
 <?php
 $soap = new SoapClient("myxml.wsdl");
-echo $soap->getPuzzle("1");
-?>
+$user= array('username'=>'new_user','password'=>'new_ganesh');
+try {
+    if( $soap->create($user) !=0){
+        echo "\n Creation successful \n";
+    }
+    else {
+        echo "\n Creation error \n";
+        return;
+    }
+//    echo "<pre>";
+    $token=$soap->login($user);
+  if($token !=0 ){
+      echo "\n Logged in successfully \n";
+  }
+    else {
+        echo "\n login error\n ";
+   }
+    $userDetails= array('username'=>'new_user1','password'=>'new_ganesh1');
+    $userToken= array('token'=>$token);
+    $user['user']=$userDetails;
+    $user['token']=$userToken;
+   if($soap->update($user) !=0 ){
+       echo "\n profile update successful  \n";
+   }
+    else{
+        echo "\n profile update error \n ";
+    }
 
+    if($soap->delete($token) !=0){
+        echo "\n profile deleted successfully ";
+    }
+    else{
+        echo "\n deletion error \n";
+    }
+}
+catch (Exception $e){
+    var_dump($e->getMessage);
+}
---
+++ soapclient/server.php
@@ -0,0 +1,86 @@
+<?php
+function getPuzzle($difficulty) {
+    $puzzles[1][] = "What is the best web mag in the world?";
+    $puzzles[2][] = "What is the air-speed velocity of an unladen swallow?";
+    $puzzles[3][] = "What is the meaning of life?";
+    $randpuz = array_rand($puzzles[$difficulty]);
+    return $puzzles[$difficulty][$randpuz];
+}
+
+function create($user) {
+    $link = mysql_connect('localhost', 'root', 'webonise6186');
+
+    if (!$link) {
+        return 0;
+    }
+    $db_selected=mysql_select_db('soapserver',$link);
+    if (!$db_selected) {
+        return 0;
+    }
+    $username=$user['username'];
+    $password=$user['password'];
+    $token=mt_rand(50,100);
+    $query="INSERT  INTO users  SET username='$username',password='$password' ,token=$token ";
+    $result=mysql_query($query);
+    return mysql_insert_id();
+}
+
+function login($userDetails) {
+    $link = mysql_connect('localhost', 'root', 'webonise6186');
+
+    if (!$link) {
+        return "could not connect";
+    }
+    $db_selected=mysql_select_db('soapserver',$link);
+    if (!$db_selected) {
+        return "Not able to select database";
+    }
+    $username=$userDetails['username'];
+    $password=$userDetails['password'];
+    $query="SELECT * FROM  users WHERE username= '$username' AND password= '$password' ";
+    $result=mysql_query($query);
+    $new=array();
+    while ($row = mysql_fetch_array($result, MYSQL_NUM)) {
+        $new=$row[3];
+    }
+    return $new;
+}
+
+function update($userDetails) {
+    $link = mysql_connect('localhost', 'root', 'webonise6186');
+
+    if (!$link) {
+        return 0;
+    }
+    $db_selected=mysql_select_db('soapserver',$link);
+    if (!$db_selected) {
+        return 0;
+    }
+    $token=$userDetails['token']['token'];
+    $username=$userDetails['user']['username'];
+    $password=$userDetails['user']['password'];
+    $query="UPDATE users  SET username='$username' ,password='$password' WHERE token= $token ";
+    $result=mysql_query($query);
+    return $result;
+}
+function delete($token) {
+    $link = mysql_connect('localhost', 'root', 'webonise6186');
+
+    if (!$link) {
+        return 0;
+    }
+    $db_selected=mysql_select_db('soapserver',$link);
+    if (!$db_selected) {
+        return 0;
+    }
+    $query="DELETE  FROM users  WHERE token= '$token'";
+    $result=mysql_query($query);
+    return $result;
+}
+
+$server = new SoapServer("myxml.wsdl");
+$server->addFunction(array("login","getPuzzle","create","update","delete"));
+$server->handle();
+
+
+?>
